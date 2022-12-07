2c7194e35fa9b615013995625ae2e46234111257
Zoe Straw <zoe.olive.straw@gmail.com>
2022-12-07 11:44:38 -0500

    Working on DB scripts

---
+++ DB Scripts/db_conn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PressStart_DBMS.DB_Scripts
+{
+    class db_conn
+    {
+        public string connectionString;
+        public SqlConnection conn;
+
+        public void Initialize()
+        {
+            conn = new SqlConnection(connectionString);
+            conn.Open();
+        }
+
+        public void Close()
+        {
+            conn.Close();
+        }
+        
+    }
+}
